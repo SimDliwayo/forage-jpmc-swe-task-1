0935e2c2e43724ab83ff19f70fc16b4af689ae02
Simphiwe Dliwayo <dliwayosim@gmail.com>
2024-07-28 04:24:32 +0200

    Fixed the client side to analys stocks according to requirements

--- client3.py
+++ client3.py
@@ -33,16 +33,20 @@ def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
     stock = quote['stock']
-    bid_price = float(quote['top_bid']['price'])
-    ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    bid_price = float(quote['top_bid']['price']) if quote['top_bid'] else None
+    ask_price = float(quote['top_ask']['price']) if quote['top_ask'] else None
+    """Checks if top_bid and top_ask exist before accessing their price key, if they don't exist it sets the price to None"""
+    price = (bid_price + ask_price) / 2 if bid_price and ask_price else None
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    # Handling None value. 
+    if price_a is not None and price_b is not None and price_b != 0:
+        return price_a / price_b
+    return None 
 
 
 # Main
@@ -52,8 +56,11 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            prices[stock] = price
+            print(f"Quoted {stock} at (bid:{bid_price}, ask:{ask_price}, price:{price})")
 
-        print("Ratio %s" % getRatio(price, price))
+        ratio = getRatio(prices.get('ABC'), prices.get('DEF'))
+        print(f"Ratio: {ratio if ratio is not None else 'N/A'}")
